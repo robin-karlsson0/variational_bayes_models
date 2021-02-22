cb81f85b246dbbe55b85f814c53d34bacae94e1e
Robin Karlsson <robin.karlsson0@gmail.com>
2021-02-22 11:23:08 +0900

    Updated linear regression model

--- variational_linear_regression/var_lin_reg.py
+++ variational_linear_regression/var_lin_reg.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import special
 from sklearn import preprocessing
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
 
 np.random.seed(seed=0)
+
 
 class VarLinRegModel():
     '''
     '''
-    def __init__(self, basis_func, iter_max:int=200, conv_criteria:float=1e-6, log_iter_freq:int=1):
+    def __init__(self, basis_func_deg, iter_max:int=200, conv_criteria:float=1e-6, log_iter_freq:int=1):
         '''
+
+        mu: (D,1) vector
+        sigma: (D,D) matrix
+
         Args:
             basis_func: 
             iter_max:
             conv_criteria:
             log_iter_freq:
         '''
-        self.basis_func = basis_func
         self.iter_max = iter_max
         self.conv_criteria = conv_criteria
         self.log_iter_freq = log_iter_freq
@@ -30,50 +35,44 @@ class VarLinRegModel():
         self.sigma = None
         self.beta = None
 
-        # Basis feature normalization object
+        # Basis feature generation object
+        self.basis_func_deg = basis_func_deg
+        self.basis_func = PolynomialFeatures(degree=basis_func_deg)
+
+        # Input and basis feature normalization object
+        self.input_scaler = None
         self.basis_scaler = None
 
-    def train(self, D):
+
+    def train(self, dataset):
         '''
         Args:
-            D: Dataset tuple (X, Y) consisting of
-                  Data matrix X: (#input features M, #samples N)
-                  Target vector Y: (#samples N, 1)
+            dataset: Dataset tuple (X, Y) consisting of
+                     Data matrix X: (#samples N, #input features M)
+                     Target vector Y: (#samples N, 1)
         '''
         # Extract data matrix 'X' (#feat M, #samples N) and label vector 'Y' (#samples N, 1)
-        X = D[0]
-        Y = D[1]
+        X = dataset[0]
+        Y = dataset[1]
 
         # Number of samples
-        N = X.shape[1]
+        samples_N = X.shape[0]
 
-        # Obtain length of basis vector according to the basis function
-        D = self.basis_func(X[:,0]).shape[0]
+        # Basis feature matrix (#samples N, #basis features D)
+        phis = self.convert_input_to_basis_matrix(X)
 
-        # Transform 'input vectors' x --> 'feature vectors' phi
-        phis = np.empty((D, N), dtype=np.float)
-        for sample_idx in range(X.shape[1]):
-            phis[:,sample_idx:sample_idx+1] = self.basis_func(X[:,sample_idx])
-
-        # Normalize 'feature matrix' phis
-        self.basis_scaler = preprocessing.StandardScaler().fit(phis.T)
-        phis = self.basis_scaler.transform(phis.T).T
-        # Recover bias term (as it gets normalized by the scaler)
-        phis[-1,:] = 1.
+        D = phis.shape[1]
 
         print("\nSummary of training data")
         print(f"  Basis matrix 'phis' : {phis.shape}")
         print(f"  Label vector 'Y'    : {Y.shape}")
-        print(f"    #Samples        : {N}")
-        print(f"    #Input features : {X.shape[0]}")
+        print(f"    #Samples        : {samples_N}")
+        print(f"    #Input features : {X.shape[1]}")
         print(f"    #Basis feat.    : {D}")
 
         ###################
         #  Training loop
         ###################
-
-        # Model formulated in terms of a (N, D) feature matrix
-        phis = phis.T
 
         # Initial variables - hyperpriors for Gamma distributions
         a_0 = 1.
@@ -87,7 +86,7 @@ class VarLinRegModel():
         d_N = d_0
 
         mu_N = np.random.random((D,1))
-        sigma_N = np.eye(D)
+        sigma_N = np.random.random((D,D))
 
         lower_bound_prev = np.inf
     
@@ -102,14 +101,14 @@ class VarLinRegModel():
             # Maximization step
             mu_N, sigma_N = self.estim_qw_distr(phis, Y, exp_alpha, exp_beta)
             a_N, b_N = self.estim_qalpha_distr(a_0, b_0, exp_quad_w, D)
-            c_N, d_N = self.estim_qbeta_distr(c_0, d_0, phis, Y, mu_N, sigma_N, obs_N)
+            c_N, d_N = self.estim_qbeta_distr(c_0, d_0, phis, Y, mu_N, sigma_N, samples_N)
             
             # Lower bound
-            lower_bound = -self.comp_lower_bound(phis, Y, mu_N, sigma_N, exp_beta, a_0, b_0, c_0, d_0, a_N, b_N, c_N, d_N, D)
+            lower_bound = self.comp_lower_bound(phis, Y, mu_N, sigma_N, exp_beta, a_0, b_0, c_0, d_0, a_N, b_N, c_N, d_N, D, samples_N)
             self.lower_bound_list.append(lower_bound)
 
             if iter_idx % self.log_iter_freq == 0:
-                print(f"  iter {iter_idx} | {lower_bound}")
+                print(f"  iter {iter_idx} | {lower_bound} | beta: {self.beta_expectation(c_N, d_N):.4f}")
 
             # Check convergence
             if (np.abs(lower_bound - lower_bound_prev) < self.conv_criteria):
@@ -127,43 +126,112 @@ class VarLinRegModel():
 
         return self.lower_bound_list
 
-
     def predictive_posterior_distr(self, x):
         '''Computes the probability distribution representing the output value of a feature vector.
+
+        mu = mu_N^T * phi
+        sigma^2 = 1/beta + phi^T * Sigma_N * phi
         
         Args:
             x: Input vector of shape (m,1).
         
         Returns:
-            mu, sigma2: Gaussian distribution parameters representing the predicted data generating process (output value 'y' given input 'x').
+            mu, sigma^2: Gaussian distribution parameters representing the predicted data generating process (output value 'y' given input 'x').
         '''
-        # Transform 'input vec' --> 'feat vec'
-        phi = self.basis_func(x)
-        # Normalize 'feat vec' similar to training
-        phi = self.basis_scaler.transform(phi.T).T
-        # Recover bias term (as it gets normalized by the scaler)
-        phi[-1,:] = 1.
+        # Basis function require (1,m) input vector
+        x = x.T
+        # Transform 'input vec' (1,m) --> 'feat vec' (1,d)
+        phi = self.convert_input_to_basis_matrix(x)
+        # Need 'phi' as (D,1) vector
+        phi = phi.T
         
         mu = self.mu.T.dot(phi)
         sigma2 = 1./self.beta + phi.T.dot(self.sigma).dot(phi)
-        return mu, sigma2
-
+        return mu[0,0], sigma2[0,0]
 
     @staticmethod
-    def comp_lower_bound(phis, ts, mu_N, sigma_N, exp_beta, a_0, b_0, c_0, d_0, a_N, b_N, c_N, d_N, D):
-        
-        term_1 = 0.5 * np.log(exp_beta / (2.*np.pi)) - 0.5 * exp_beta * ts.T.dot(ts) + exp_beta * mu_N.T.dot(phis.T).dot(ts) - 0.5 * exp_beta * np.trace(phis.T.dot(phis).dot( mu_N.dot(mu_N.T) + sigma_N ))
-        term_2 = -0.5*D * np.log(2.*np.pi) + 0.5*D*(special.digamma(a_N) - np.log(b_N)) - (a_N / (2.*b_N)) * (mu_N.T.dot(mu_N) + np.trace(sigma_N))
-        term_3 = a_0 * np.log(b_0) + (a_0 - 1.)*(special.digamma(a_N) - np.log(b_N)) - b_0*(a_N/b_N) - np.log(special.gamma(a_N))
-        term_4 = c_0 * np.log(d_0) + (c_0 - 1.)*(special.digamma(c_N) - np.log(d_N)) - d_0*(c_N/d_N) - np.log(special.gamma(c_N))
-        term_5 = -0.5*np.log(np.linalg.det(sigma_N)) - 0.5*D*(1. + np.log(2.*np.pi))
-        term_6 = -np.log(special.gamma(a_N)) + (a_N - 1.)*special.digamma(a_N) + np.log(b_N) - a_N
-        term_7 = -np.log(special.gamma(c_N)) + (c_N - 1.)*special.digamma(c_N) + np.log(d_N) - c_N
-        
+    def comp_lower_bound(phis, ts, mu_N, sigma_N, beta, a_0, b_0, c_0, d_0, a_N, b_N, c_N, d_N, D, N, eps=1e-14):
+        # Exp[ log p(t|w) ]_{w}
+        term_1 = 0.5 * N * np.log(beta / (2.*np.pi) + eps) - 0.5 * beta * ts.T.dot(ts) + beta * mu_N.T.dot(phis.T).dot(ts) - 0.5 * beta * np.trace(phis.T.dot(phis).dot( mu_N.dot(mu_N.T) + sigma_N ))
+        # Exp[ log p(w|alpha) ]_{w, alpha}
+        term_2 = -0.5 * D * np.log(2.*np.pi) + 0.5*D*(special.digamma(a_N) - np.log(b_N + eps)) - (a_N / (2.*b_N)) * (mu_N.T.dot(mu_N) + np.trace(sigma_N))
+        # Exp[ log p(alpha) ]_{alpha}
+        term_3 = a_0 * np.log(b_0 + eps) + (a_0 - 1.)*(special.digamma(a_N) - np.log(b_N + eps)) - b_0*(a_N/b_N) - np.log(special.gamma(a_N) + eps)
+        # Exp[ log p(beta) ]_{beta}
+        term_4 = c_0 * np.log(d_0 + eps) + (c_0 - 1.)*(special.digamma(c_N) - np.log(d_N + eps)) - d_0*(c_N/d_N) - np.log(special.gamma(c_N) + eps)
+        # Exp[ log q(w) ]_{w}
+        term_5 = -0.5*np.log(np.linalg.det(sigma_N) + eps) - 0.5*D*(1. + np.log(2.*np.pi))
+        # Exp[ log q(alpha) ]_{alpha}
+        term_6 = -np.log(special.gamma(a_N) + eps) + (a_N - 1.)*special.digamma(a_N) + np.log(b_N + eps) - a_N
+        # Exp[ log q(beta) ]_{beta}
+        term_7 = -np.log(special.gamma(c_N) + eps) + (c_N - 1.)*special.digamma(c_N) + np.log(d_N + eps) - c_N
         lower_bound = term_1 + term_2 + term_3 + term_4 - term_5 - term_6 - term_7
         # Return scalar
-        return lower_bound[0,0] 
+        return lower_bound.item()
 
+    def evaluate(self, dataset):
+        '''Evaluates a trained model on a dataset according to errors and MSE.
+        Args:
+            dataset: Tuple (X, Y) consisting of
+                Data matrix X:   (#samples N, #input features M)
+                Target vector Y: (#samples N, 1)
+        Returns:
+            Tuple of errors and MSE
+                errors: Array of signed errors (prediction - target)
+                MSE:    Scalar
+        '''
+        # Extract data matrix 'X' (#feat M, #samples N) and label vector 'Y' (#samples N, 1)
+        X = dataset[0]
+        Y = dataset[1]
+
+        # Number of samples
+        samples_N = X.shape[0]
+
+        #####################
+        #  Evaluation loop
+        #####################
+        errors = []
+        MSE = []
+        for sample_idx in range(samples_N):
+
+            x = X[sample_idx:sample_idx+1, :].T
+            mu_out, _ = self.predictive_posterior_distr(x)
+
+            y_target = Y[sample_idx,0]
+
+            diff = mu_out - y_target
+            errors.append(diff)
+            MSE.append(diff**2)
+        
+        errors = np.array(errors)
+        MSE = np.sum(np.array(MSE)) / len(MSE)
+
+        return errors, MSE
+
+    def convert_input_to_basis_matrix(self, X):
+        '''Returns the basis feature matrix 'phis' and fits basis cunc
+        Args:
+            X: Input matrix of shape (#samples N, #input features M)
+        '''
+        # Normalize 'input matrix' X
+        if self.input_scaler == None:
+            self.input_scaler = preprocessing.StandardScaler().fit(X)
+
+        X_norm = self.input_scaler.transform(X)
+
+        # Transform 'input vectors' x --> 'feature vectors' phi 
+        phis = self.basis_func.fit_transform(X_norm)
+
+        # Normalize 'feature matrix' phis
+        if self.basis_scaler == None:
+            self.basis_scaler = preprocessing.StandardScaler().fit(phis)
+
+        phis = self.basis_scaler.transform(phis)
+          
+        # Recover bias term (as it gets normalized by the scaler)
+        phis[:,0] = 1.
+
+        return phis
 
     ##################
     #  Expectations
@@ -195,7 +263,8 @@ class VarLinRegModel():
         
         Ref: Bishop Eq 10.183 (p.505)
         '''
-        return np.matmul(mu_N.T, mu_N) + np.trace(sigma_N) 
+        val = np.dot(mu_N.T, mu_N) + np.trace(sigma_N)
+        return val.item() 
 
     ############################################
     #  Maximization (re-estimation) equations
@@ -206,8 +275,8 @@ class VarLinRegModel():
         '''Returns parameters for the re-estimated distribution which maximizes the lower bound.
         
         Args:
-            phis:   : Feature matrix of shape (D,N).
-            ts      : Label vector of shape (1,N).
+            phis:   : Feature matrix of shape (N, D).
+            ts      : Label vector of shape (N, 1).
             exp_alpha : Expected value of Gamma distribution modeling variance of 'p(w)'.
             exp_beta  : Expected value of Gamma distribution modeling variance of 'p(t)'.
             
@@ -216,13 +285,13 @@ class VarLinRegModel():
         
         Ref: Bishop Eq. 10.96-10.98 (p.487)
         '''
-        sigma_N = exp_beta * phis.T.dot(phis) + exp_alpha * np.eye(phis.shape[1])
+        feature_dim = phis.shape[1]
+        sigma_N = exp_beta * phis.T.dot(phis) + exp_alpha * np.eye(feature_dim)
         sigma_N = np.linalg.inv(sigma_N)
-        
+
         mu_N = exp_beta * sigma_N.dot(phis.T).dot(ts)
         
         return mu_N, sigma_N
-
 
     @staticmethod
     def estim_qalpha_distr(a_0, b_0, exp_quad_w, D):
@@ -243,7 +312,6 @@ class VarLinRegModel():
         b_N = b_0 + 0.5*exp_quad_w
         
         return a_N, b_N
-
 
     @staticmethod
     def estim_qbeta_distr(c_0, d_0, phis, ts, mu_N, sigma_N, N):
@@ -266,23 +334,14 @@ class VarLinRegModel():
         
         return c_N, d_N
 
-
-def basis_func_poly(x):
-    '''Returns a feature vector 'phi' by transforming input vector 'x' according to a basis function.
-    '''
-    feat_vec = np.zeros((4,1), dtype=np.float)
-
-    feat_vec[0,0] = x
-    feat_vec[1,0] = x**2
-    feat_vec[2,0] = x**3
-    feat_vec[3,0] = 1.0
-
-    return feat_vec
+    def get_basis_func_deg(self):
+        return self.basis_func_deg
 
 
 if __name__ == "__main__":
 
-    model = VarLinRegModel(basis_func_poly)
+    basis_func_deg = 3
+    model = VarLinRegModel(basis_func_deg, iter_max=200)
 
     #################################
     #  Generate dataset D = (X, Y)
@@ -304,25 +363,24 @@ if __name__ == "__main__":
     x_dense = np.linspace(x_range_min, x_range_max, 200)
     y_true = np.sin(np.pi*x_dense)
 
-    X = np.expand_dims(x, 0)
+    X = np.expand_dims(x, 0).T
     Y = np.expand_dims(y, 1)
 
     print("\nSummary of dataset")
     print(f"  Data matrix X : {X.shape}")
     print(f"  Label vector Y: {Y.shape}")
 
-    # Normalize 'input vectors' x using statistics calculated for each feature (rows)
-    X_mean = np.mean(X, axis=1, dtype=np.float64)
-    X_std = np.std(X, axis=1, dtype=np.float64)
-    X = (X - X_mean) / X_std
-
-    D = (X, Y)
+    dataset = (X, Y)
 
     #################
     #  Train model
     #################
     
-    model.train(D)
+    model.train(dataset)
+
+    errors, MSE = model.evaluate(dataset)
+    print(f"errors: {errors}")
+    print(f"RMSE: {np.sqrt(MSE)}")
 
     #####################
     #  Model inference
@@ -335,9 +393,7 @@ if __name__ == "__main__":
 
     for i in range(x_array.shape[0]):
 
-        # Remember to normalize the input feature vector like the training data
-        x_norm = (x_array[i] - X_mean) / X_std
-        
+        x_norm = np.array([[x_array[i]]])
         mu_i, sigma2_i = model.predictive_posterior_distr(x_norm)
         
         mu_array[i] = mu_i
